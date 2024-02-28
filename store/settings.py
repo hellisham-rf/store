fb8e8730e12d72ef6120b7580db973039e31c437
hellisham-rf <ashkan.m.m.t2@gmail.com>
2024-02-28 12:34:45 +0330

    sec commit

--- store/settings.py
+++ store/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
-import environ
+SECRET_KEY="django-insecure-muq84_o$sk2shvtqw!6t3^0htz^u!f6kfv+a65!17inis=a^o#"
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,8 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-from decouple import config
-SECRET_KEY=config("SECRET_KEY")
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
